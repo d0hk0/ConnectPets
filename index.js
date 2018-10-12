ef97d810f17c79adbebaba3ac676deef6ac04250
Alexis Cuellar <darkurt@gmail.com>
2018-10-12 00:24:54 -0300

    ajustes html

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
 var http = require('http');
 var fs = require('fs');
+var port = process.env.PORT || 1337;
 
 fs.readFile('./html/index.html', function (err, html) {
     if (err) {
         throw err; 
-    }
- };
+    }       
+    server = http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(port);
+  });
 
-
-
-var server = http.createServer(function(request, response) {
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write(html); 
-    response.end();
-});
-
-
-
-var port = process.env.PORT || 1337;
-server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
