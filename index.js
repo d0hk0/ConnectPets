886bc054ac7b9bae858c05b2588d34446be16a5a
Alexis Cuellar <darkurt@gmail.com>
2018-10-12 00:03:12 -0300

    ajuste para archivo html

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 var http = require('http');
+var fs = require('fs');
+
+fs.readFile('./html/index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    } 
+
+
 
 var server = http.createServer(function(request, response) {
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+	
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(html); 
+    response.end();
+
 
 });
+
+
 
 var port = process.env.PORT || 1337;
 server.listen(port);
